af0981c3507a2748d8d33c1b596e9a07ab7eea99
Milind Mishra <milind.mishra4@gmail.com>
2025-05-02 02:02:10 +0530

    feat: init tailwind and shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import './App.css'
-
-
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div className="text-center mt-5">
       <h1>Hello World</h1>
       <p>This is your custom new tab page!</p>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import path from "path";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: "public/manifest.json",
-					dest: ".",
-				},
-				// {
-				// 	src: "public/*.png",
-				// 	dest: ".",
-				// },
-			],
-		}),
-	],
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: {
-				main: "index.html",
-			},
-		},
-	},
+  plugins: [
+    react(),
+    // @ts-expect-error : tailwindcss is not typed
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+        // {
+        // 	src: "public/*.png",
+        // 	dest: ".",
+        // },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
 });
