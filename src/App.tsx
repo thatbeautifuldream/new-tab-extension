2b9082f21d38016b6da2aba10522d9c8d3663b15
Milind Mishra <milind.mishra4@gmail.com>
2025-05-01 23:46:05 +0530

    init new tab extension

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World</h1>
+      <p>This is your custom new tab page!</p>
+    </div>
+  );
+}
+
+export default App;
